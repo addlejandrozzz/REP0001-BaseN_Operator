27a851a15c122266457282bea46d69b79949d31f
Adolfo Alejandro Arenas Ramos <adolfoalejandroar@ufps.edu.co>
2025-03-11 08:24:15 -0500

    adding and developing the hexa and octal conversors

--- ncs/src/main/java/exfora/model/conversors/Hexa_Conversor.java
+++ ncs/src/main/java/exfora/model/conversors/Hexa_Conversor.java
@@ -2,20 +2,32 @@ package exfora.model.conversors;
 
 public class Hexa_Conversor {
     public static String toBinary(String x) {
-        long number = Long.parseLong(x);
-
-        return null;
+        long number = Long.parseLong(x, 16);
+        StringBuilder binary = new StringBuilder();
+        while (number > 0) {
+            binary.insert(0, number % 2);
+            number = number / 2;
+        }
+        return binary.toString();
     }
 
     public static String toOctal(String x) {
-        long number = Long.parseLong(x);
-
-        return null;
+        long number = Long.parseLong(x, 16);
+        StringBuilder octal = new StringBuilder();
+        while (number > 0) {
+            octal.insert(0, number % 8);
+            number = number / 8;
+        }
+        return octal.toString();
     }
 
     public static String toDecimal(String x) {
-        long number = Long.parseLong(x);
-
-        return null;
+        long number = 0;
+        for (int i = 0; i < x.length(); i++) {
+            char hexChar = x.charAt(i);
+            int decimalValue = Character.digit(hexChar, 16);
+            number = number * 16 + decimalValue;
+        }
+        return Long.toString(number);
     }
 }
--- ncs/src/main/java/exfora/model/conversors/Octal_Conversor.java
+++ ncs/src/main/java/exfora/model/conversors/Octal_Conversor.java
@@ -2,20 +2,38 @@ package exfora.model.conversors;
 
 public class Octal_Conversor {
     public static String toBinary(String x) {
-        long number = Long.parseLong(x);
-
-        return null;
+        int decimal = Integer.parseInt(toDecimal(x));
+        StringBuilder binary = new StringBuilder();
+        while (decimal > 0) {
+            binary.insert(0, decimal % 2);
+            decimal = decimal / 2;
+        }
+        return binary.toString();
     }
 
     public static String toDecimal(String x) {
-        long number = Long.parseLong(x);
-
-        return null;
+        int decimal = 0;
+        int length = x.length();
+        for (int i = 0; i < length; i++) {
+            char c = x.charAt(length - 1 - i);
+            int digit = Character.getNumericValue(c);
+            decimal += digit * Math.pow(8, i);
+        }
+        return String.valueOf(decimal);
     }
 
     public static String toHexadecimal(String x) {
-        long number = Long.parseLong(x);
-
-        return null;
+        int decimal = Integer.parseInt(toDecimal(x));
+        StringBuilder hexadecimal = new StringBuilder();
+        while (decimal > 0) {
+            int remainder = decimal % 16;
+            if (remainder < 10) {
+                hexadecimal.insert(0, (char) ('0' + remainder));
+            } else {
+                hexadecimal.insert(0, (char) ('A' + (remainder - 10)));
+            }
+            decimal = decimal / 16;
+        }
+        return hexadecimal.toString();
     }
 }
